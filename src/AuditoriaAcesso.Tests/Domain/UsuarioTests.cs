0baa95005abd0aa369f71fdb85e4ae1be6eb6869
Helio Carrara <heliocarrara@gmail.com>
2026-06-11 13:20:35 -0400

    test: adicionar testes unitarios de dominio para exclusao de usuario

---
+++ src/AuditoriaAcesso.Tests/Domain/UsuarioTests.cs
@@ -0,0 +1,29 @@
+﻿using AuditoriaAcesso.Domain.Entities;
+
+namespace AuditoriaAcesso.Tests.Domain;
+
+public class UsuarioTests
+{
+    [Fact]
+    public void Deve_Permitir_Exclusao_Usuario_Sem_Logs()
+    {
+        var usuario = new Usuario("João Silva", "joao@email.com", "senha_hash_123");
+
+        var podeSerExcluido = usuario.PodeSerExcluido();
+
+        Assert.True(podeSerExcluido);
+    }
+
+    [Fact]
+    public void Não_Deve_Permitir_Exclusao_Usuario_Com_Logs()
+    {
+        var usuario = new Usuario("João Silva", "joao@email.com", "senha_hash_123");
+        usuario.RegistrarAcesso("192.168.1.1.");
+
+        var podeSerExcluido = usuario.PodeSerExcluido();
+
+        Assert.False(podeSerExcluido);
+    }
+
+
+}
